2f113d1740107f9463c4544b67f95b6a53733b83
Blu <swagnemite@gmx.de>
2018-03-10 23:20:11 +0100

    hitscan bullets with filter + t90ms sound scripts

--- lua/autorun/server/armedvehicles_handler.lua
+++ lua/autorun/server/armedvehicles_handler.lua
@@ -38,6 +38,15 @@ sound.Add( {
 } )
 
 sound.Add( {
+	name = "t90ms_fire",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 140,
+	pitch = { 90, 110 },
+	sound = "^simulated_vehicles/weapons/t90ms_cannon.wav"
+} )
+
+sound.Add( {
 	name = "tiger_fire_mg",
 	channel = CHAN_STATIC,
 	volume = 1.0,
@@ -80,6 +89,15 @@ sound.Add( {
 	level = 70,
 	pitch = { 90, 110 },
 	sound = "simulated_vehicles/weapons/sherman_reload.wav"
+} )
+
+sound.Add( {
+	name = "t90ms_reload",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 70,
+	pitch = { 90, 110 },
+	sound = "simulated_vehicles/weapons/t90ms_reload.wav"
 } )
 
 sound.Add( {
@@ -134,6 +152,64 @@ function simfphys.FirePhysProjectile( data )
 	projectile.Filter = filter
 	projectile:Spawn()
 	projectile:Activate()
+end
+
+function simfphys.FireHitScan( data )
+	if not data then return end
+	if not istable( data.filter ) then return end
+	if not isvector( data.shootOrigin ) then return end
+	if not isvector( data.shootDirection ) then return end
+	if not IsValid( data.attacker ) then return end
+	if not IsValid( data.attackingent ) then return end
+	
+	data.Spread = data.Spread or Vector(0,0,0)
+	data.Tracer = data.Tracer or 0
+	data.HullSize = data.HullSize or 1
+	
+	local trace = util.TraceHull( {
+		start = data.shootOrigin,
+		endpos = data.shootOrigin + (data.shootDirection + Vector(math.Rand(-data.Spread.x,data.Spread.x),math.Rand(-data.Spread.y,data.Spread.y),math.Rand(-data.Spread.x,data.Spread.x)) )* 50000,
+		filter = data.filter,
+		maxs = data.HullSize,
+		mins = -data.HullSize
+	} )
+	
+	local bullet = {}
+	bullet.Num 			= 1
+	bullet.Src 			= trace.HitPos - data.shootDirection * 5
+	bullet.Dir 			= data.shootDirection
+	bullet.Spread 		= Vector(0,0,0)
+	bullet.Tracer		= 0
+	bullet.Force		= (data.Force and data.Force or 1)
+	bullet.Damage		= (data.Damage and data.Damage or 1)
+	bullet.HullSize		= data.HullSize
+	bullet.Attacker 		= data.attacker
+	bullet.Callback = function(att, tr, dmginfo)
+		if tr.Entity ~= Entity(0) then
+			if simfphys.IsCar( tr.Entity ) then
+				local effectdata = EffectData()
+					effectdata:SetOrigin( tr.HitPos + tr.HitNormal )
+					effectdata:SetNormal( tr.HitNormal )
+				util.Effect( "stunstickimpact", effectdata, true, true )
+			
+				sound.Play( Sound( "weapons/fx/rics/ric"..math.random(1,5)..".wav" ), tr.HitPos, 60)
+			end
+		end
+	end
+	data.attackingent:FireBullets( bullet )
+	
+	data.attackingent.hScanTracer = data.attackingent.hScanTracer and (data.attackingent.hScanTracer + 1) or 0
+	
+	if data.Tracer > 0 then
+		if data.attackingent.hScanTracer >= data.Tracer then 
+			data.attackingent.hScanTracer = 0
+			
+			local effectdata = EffectData()
+			effectdata:SetStart( data.shootOrigin ) 
+			effectdata:SetOrigin( trace.HitPos )
+			util.Effect( "simfphys_tracer", effectdata )
+		end
+	end
 end
 
 function simfphys.RegisterCrosshair( ent, data )
